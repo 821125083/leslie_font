9f6f5d6764cca2299e6abb68455a47d460e259a0
821125083 <821125083@qq.com>
2020-11-13 20:44:28 +0800

    [fix]

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <h1>国内疫情数据</h1>
-        <el-card class="box-card" >
+        <h1>抗击肺炎</h1>
+        <div id="myChart" :style="{width: '300px', height: '300px'}">
+
+        </div>
+
+        <!-- <el-card class="box-card" >
             <div slot="header" class="clearfix">
                 <span>实时{{cov19Record.updateTime}}数据</span>
                 <el-button style="float: right; padding: 3px 0" type="text" @click="getRealTimeRecord()">获取实时数据</el-button>
             </div>
+
             <el-tag>
             确诊人数：{{cov19Record.confirmedCount}}
             </el-tag>
@@ -13,6 +18,7 @@
             较昨日新增确诊人数：{{cov19Record.confirmedAdd}}
             </el-tag>
             <br>
+
             <el-tag>
             今日确诊人数：{{cov19Record.currentConfirmedCount}}
             </el-tag>
@@ -20,6 +26,7 @@
             较昨日新增确诊：{{cov19Record.currentConfirmedAdd}}        
             </el-tag>
             <br>
+
             <el-tag type="warning">
             疑似病例：{{cov19Record.suspectedCount}}        
             </el-tag>
@@ -27,6 +34,7 @@
             较昨日新增疑似病例：{{cov19Record.suspectedAdd}}        
             </el-tag>
             <br>
+
             <el-tag>
             重症人数：{{cov19Record.severecasesCount}}        
             </el-tag>
@@ -34,6 +42,7 @@
             较昨日新增重症人数：{{cov19Record.severecasesAdd}}        
             </el-tag>
             <br>
+
             <el-tag type="success">
             痊愈人数：{{cov19Record.curedCount}}        
             </el-tag>
@@ -41,6 +50,7 @@
             较昨日新增痊愈人数：{{cov19Record.curedAdd}}        
             </el-tag>
             <br>
+
             <el-tag type="danger">
             死亡人数：{{cov19Record.deadCount}}        
             </el-tag>
@@ -53,18 +63,7 @@
             <el-tag>
             消息详情：{{cov19Record.description}}        
             </el-tag>
-        </el-card>
-
-        <el-select v-model="selectProvince" placeholder="请选择查询省份">
-            <el-option  v-for="province in provinces" 
-                        :key="province.id" 
-                        :value="province.provinceName" 
-                        :label="province.provinceName">
-            </el-option>
-        </el-select>
-        <el-table>
-
-        </el-table>
+        </el-card> -->
 
 
     </div>
@@ -72,35 +71,56 @@
 
 <script>
 
+import father from '@/components/father';
+import son from '@/components/son';
+import son2 from '@/components/son2';
+
 
 export default {
     components:{
+        father,son,son2
     },
     data(){
         return {
+            password:'13',
+            students:[],
             //使用全局定义的变量
             url:this.url.baseUrl,
             testUrl:this.url.testUrl,
-            cov19Record:Object,
-            provinces:[],
-            selectProvince:''
+            cov19Record:null
         }
     },
     //渲染之前就预先加载数据
     mounted(){
-        this.queryProvince();
+        // this.getRealTimeRecord();
+        this.drawLine();
+
+             
     },
     methods:{
+        drawLine(){
+            // 基于准备好的dom，初始化echarts实例
+            let myChart = this.$echarts.init(document.getElementById('myChart'))
+            // 绘制图表
+            myChart.setOption({
+                title: { text: '在Vue中使用echarts' },
+                tooltip: {},
+                xAxis: {
+                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+        },
         getRealTimeRecord(){
             this.$axios(this.testUrl+'Cov19Cn/realTimeRecord').then(res=>{
                 this.cov19Record = res.data;
             }) 
         },
-        queryProvince(){
-            this.$axios(this.testUrl+'location/allProvinces').then(res=>{
-                this.provinces = res.data;
-            }) 
-        }
         
       
     }
