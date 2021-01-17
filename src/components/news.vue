4d4855c88c876879a2547defe4380c4f84c2b570
821125083 <821125083@qq.com>
2021-01-17 22:03:17 +0800

    [fix]

--- src/components/news.vue
+++ src/components/news.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <el-input v-model="searchText" @change="searchTextChange()" placeholder="请输入标题" style=""></el-input>
         <el-table :data="tableData" :header-cell-style="{'text-align':'center'}" :cell-style="{'text-align':'center'}">
             <el-table-column prop="title" label="标题" >
                 <template slot-scope="row">
@@ -18,12 +19,23 @@
 export default {
     data(){
         return {
+            searchText:"",
             url : this.url.testUrl,
             tableData:[]
         }
     },
     mounted(){
-        this.$axios(this.url+'news/queryAllNews').then(res=>{
+        this.$axios(
+            {
+                url:this.url+'news/queryNewsByPage',
+                data:
+                {
+                    searchText:this.searchText ,
+                    pageSize:15,
+                    pageNum:1
+                },
+                method:'post'
+            }).then(res=>{
             this.tableData = res.data.data.newsList;
         })
     },
@@ -33,6 +45,21 @@ export default {
         // }
     },
     methods:{
+        searchTextChange(){
+            this.$axios(
+            {
+                url:this.url+'news/queryNewsByPage',
+                data:
+                {
+                    searchText:this.searchText ,
+                    pageSize:15,
+                    pageNum:1
+                },
+                method:'post'
+            }).then(res=>{
+            this.tableData = res.data.data.newsList;
+        })
+        },
         titleRow(row){
             return "<a href="+row.url+" target='_blank'>"+row.title+"</a>";
         }
